0a24bf537f725111f52f343f37d929c847da2870
shravya-byri <shravya.byri@gmail.com>
2021-04-02 19:42:34 +0530

    level order printing

--- src/dataStructure/trees.java
+++ src/dataStructure/trees.java
@@ -1,5 +1,6 @@
 package dataStructure;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -42,13 +43,31 @@ public class trees {
         return current;
 
     }
+    public static void levelOrder(Node root) {
+        ArrayDeque<Node> q = new ArrayDeque<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node current = q.poll();
+            if(current.left != null){
+                q.add(current.left);
+            }
+            if(current.right != null){
+                q.add(current.right);
+            }
+            System.out.print(current.data + " ");
+        }
+
+
+
+    }
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>(Arrays.asList(3, 1, 5, 4));
         Node root = newLeaf(a.get(0));
         for (int i = 1; i < a.size(); i++) {
             insertBST(root, a.get(i));
         }
-        recPrintTree(root);
+        levelOrder(root);
+//        recPrintTree(root);
 
     }
 }
