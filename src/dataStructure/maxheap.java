0f7120f768c8f30b418b5f1787f40873fcdce1aa
shravya-byri <shravya.byri@gmail.com>
2021-04-15 20:58:51 +0530

    max heap

--- src/dataStructure/maxheap.java
+++ src/dataStructure/maxheap.java
@@ -14,14 +14,40 @@ public class maxheap {
             heap.set(parent(current), t);
             current = parent(current);
         }
-
-
     }
     static int parent(int child) {
         return ((child - 1) / 2);
     }
-    public static void deleteHeap(ArrayList<Integer> heap, int k) {
-
+    static void swap(ArrayList<Integer> a, int i, int j) {
+        int t = a.get(i);
+        a.set(i, a.get(j));
+        a.set(j, t);
+    }
+    public static int getMax(ArrayList<Integer> heap) {
+        if (heap.isEmpty()) {
+            System.out.println("Empty Heap");
+            return -99;
+        }
+        int max = heap.get(0);
+        heap.set(0, heap.get(heap.size()-1));
+        heap.remove(heap.size()-1);
+        int current = 0;
+        while (
+                ((2 * current + 2) < heap.size()) &&
+                (
+                        heap.get(current) < heap.get(2 * current + 1) ||
+                        heap.get(current) < heap.get(2 * current + 2)
+                )
+        ) {
+            int maxChildIndex;
+            if (heap.get(2 * current + 1) > heap.get(2 * current + 2))
+                maxChildIndex = 2 * current + 1;
+            else
+                maxChildIndex = 2 * current + 2;
+            swap(heap, current, maxChildIndex);
+            current = maxChildIndex;
+        }
+        return max;
     }
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1,5,4,3,6,7));
@@ -29,7 +55,8 @@ public class maxheap {
         for (int i =0;i < a.size();i++) {
             insertHeap(h, a.get(i));
         }
-        insertHeap(h, 0);
+        //insertHeap(h, 0);
+        getMax(h);
         for (int i=0; i<h.size();i++) {
             System.out.print(h.get(i) + " ");
         }
