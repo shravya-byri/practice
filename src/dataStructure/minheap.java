56771490ce2cce84c2804ccebfbf1d7e309d276b
shravya-byri <shravya.byri@gmail.com>
2021-04-15 21:06:48 +0530

    min heap

--- src/dataStructure/minheap.java
+++ src/dataStructure/minheap.java
@@ -21,6 +21,37 @@ public class minheap {
     static int parent(int child) {
         return ((child - 1) / 2);
     }
+    static void swap(ArrayList<Integer> a, int i, int j) {
+        int t = a.get(i);
+        a.set(i, a.get(j));
+        a.set(j, t);
+    }
+    public static int getMin(ArrayList<Integer> heap) {
+        if (heap.isEmpty()) {
+            System.out.println("Empty Heap");
+            return -99;
+        }
+        int min = heap.get(0);
+        heap.set(0, heap.get(heap.size()-1));
+        heap.remove(heap.size()-1);
+        int current = 0;
+        while (
+                ((2 * current + 2) < heap.size()) &&
+                        (
+                                heap.get(current) > heap.get(2 * current + 1) ||
+                                heap.get(current) > heap.get(2 * current + 2)
+                        )
+        ) {
+            int minChildIndex;
+            if (heap.get(2 * current + 1) < heap.get(2 * current + 2))
+                minChildIndex = 2 * current + 1;
+            else
+                minChildIndex = 2 * current + 2;
+            swap(heap, current, minChildIndex);
+            current = minChildIndex;
+        }
+        return min;
+    }
     public static void deleteHeap(ArrayList<Integer> heap, int k) {
 
     }
@@ -30,7 +61,8 @@ public class minheap {
         for (int i =0;i < a.size();i++) {
             insertHeap(h, a.get(i));
         }
-        insertHeap(h, 0);
+        //insertHeap(h, 0);
+        getMin(h);
         for (int i=0; i<h.size();i++) {
             System.out.print(h.get(i) + " ");
         }
