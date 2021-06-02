cd237ee177a160a594f51763fa12ad651e1120c8
shravya-byri <shravya.byri@gmail.com>
2021-06-02 11:38:57 +0530

    fourstars

--- src/fourstars.java
+++ src/fourstars.java
@@ -1,8 +1,8 @@
 public class fourstars {
     public static void main(String[] args) {
-        for(int i=1; i<=4; i++){
-            for (int j=1; j<=4; j++){
-                if(j<=i)
+        for(int i = 1; i <= 4; i++){
+            for (int j = 1; j <= 4; j++){
+                if(j <= i)
                     System.out.print(j);
                 else
                     System.out.print("*");
